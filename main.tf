db7d04d6e7d2c8ca065cd9a91644c3909040a57d
Kristo Aun <kristoaun@live.com>
2024-01-23 16:28:01 +0200

    run

--- main.tf
+++ main.tf
@@ -32,60 +32,70 @@ locals {
   ]
 }
 
-module "container_loadbalancers" {
-  count  = length(local.containers_loadbalancer)
-  source = "github.com/studio-telephus/terraform-docker-haproxy.git?ref=main"
-  image  = data.docker_image.debian_bookworm.id
-  name   = local.containers_loadbalancer[count.index].name
-  networks = [
-    {
-      name         = local.nicparent
-      ipv4_address = local.containers_loadbalancer[count.index].ipv4_address
-    }
+module "cluster" {
+  source = "github.com/studio-telephus/terraform-docker-k3s.git?ref=main"
+  network_name  = local.nicparent
+  cluster_name  = "k3s-${var.env}"
+  server_config = [
+    "--disable", "local-storage"
   ]
-  bind_port           = local.containers_loadbalancer[count.index].bind_port
-  servers             = local.containers_loadbalancer[count.index].servers
-  stats_auth_password = module.bw_haproxy_stats.data.password
+  k3s_token = module.bw_haproxy_stats.data.password
 }
-
-module "docker_swarm" {
-  source            = "github.com/studio-telephus/terraform-docker-k3s-swarm.git?ref=main"
-  swarm_private_key = module.bw_swarm_private_key.data.notes
-  containers        = concat(local.containers_server, local.containers_worker)
-  network_name      = local.nicparent
-  restart           = "unless-stopped"
-}
-
-module "k3s_cluster" {
-  source          = "github.com/studio-telephus/terraform-docker-k3s-embedded.git?ref=main"
-  ssh_private_key = module.bw_swarm_private_key.data.notes
-  cluster_domain  = local.cluster_domain
-  cidr_pods       = "10.20.10.0/22"
-  cidr_services   = "10.20.15.0/22"
-  k3s_install_env_vars = {
-    "K3S_KUBECONFIG_MODE" = "644"
-  }
-  server_flags = [
-    "--write-kubeconfig-mode '0644'",
-    "--node-taint CriticalAddonsOnly=true:NoExecute",
-    "--kube-controller-manager-arg allocate-node-cidrs",
-    "--flannel-backend none",
-    "--disable-kube-proxy",
-    "--disable-network-policy",
-    "--disable traefik",
-    "--disable local-storage",
-    "--tls-san ${local.fixed_registration_ip}"
-  ]
-  containers_server = local.containers_server
-  containers_worker = local.containers_worker
-  depends_on = [
-    module.docker_swarm,
-    module.container_loadbalancers[0]
-  ]
-}
-
-resource "local_sensitive_file" "kube_config" {
-  content    = module.k3s_cluster.k3s_kube_config
-  filename   = var.kube_config_path
-  depends_on = [module.k3s_cluster]
-}
+//
+//module "container_loadbalancers" {
+//  count  = length(local.containers_loadbalancer)
+//  source = "github.com/studio-telephus/terraform-docker-haproxy.git?ref=main"
+//  image  = data.docker_image.debian_bookworm.id
+//  name   = local.containers_loadbalancer[count.index].name
+//  networks = [
+//    {
+//      name         = local.nicparent
+//      ipv4_address = local.containers_loadbalancer[count.index].ipv4_address
+//    }
+//  ]
+//  bind_port           = local.containers_loadbalancer[count.index].bind_port
+//  servers             = local.containers_loadbalancer[count.index].servers
+//  stats_auth_password = module.bw_haproxy_stats.data.password
+//}
+//
+//module "docker_swarm" {
+//  source            = "github.com/studio-telephus/terraform-docker-k3s-swarm.git?ref=main"
+//  swarm_private_key = module.bw_swarm_private_key.data.notes
+//  containers        = concat(local.containers_server, local.containers_worker)
+//  network_name      = local.nicparent
+//  restart           = "unless-stopped"
+//}
+//
+//module "k3s_cluster" {
+//  source          = "github.com/studio-telephus/terraform-docker-k3s-embedded.git?ref=main"
+//  ssh_private_key = module.bw_swarm_private_key.data.notes
+//  cluster_domain  = local.cluster_domain
+//  cidr_pods       = "10.20.10.0/22"
+//  cidr_services   = "10.20.15.0/22"
+//  k3s_install_env_vars = {
+//    "K3S_KUBECONFIG_MODE" = "644"
+//  }
+//  server_flags = [
+//    "--write-kubeconfig-mode '0644'",
+//    "--node-taint CriticalAddonsOnly=true:NoExecute",
+//    "--kube-controller-manager-arg allocate-node-cidrs",
+//    "--flannel-backend none",
+//    "--disable-kube-proxy",
+//    "--disable-network-policy",
+//    "--disable traefik",
+//    "--disable local-storage",
+//    "--tls-san ${local.fixed_registration_ip}"
+//  ]
+//  containers_server = local.containers_server
+//  containers_worker = local.containers_worker
+//  depends_on = [
+//    module.docker_swarm,
+//    module.container_loadbalancers[0]
+//  ]
+//}
+//
+//resource "local_sensitive_file" "kube_config" {
+//  content    = module.k3s_cluster.k3s_kube_config
+//  filename   = var.kube_config_path
+//  depends_on = [module.k3s_cluster]
+//}
--- variables.tf
+++ variables.tf
@@ -7,9 +7,3 @@ variable "kube_config_path" {
   type    = string
   default = ".terraform/kube_config.yml"
 }
-
-variable "autostart" {
-  type    = bool
-  default = true
-}
-
